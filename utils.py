aa69c1255d2df0b65ebee5a957e4f0b8abbd7765
m4ddabs <43279651+m4ddabs@users.noreply.github.com>
2023-11-28 10:46:46 +0100

    Added test model function

--- utils.py
+++ utils.py
@@ -3,7 +3,9 @@ from tslearn.datasets import UCR_UEA_datasets
 import keras_core as keras
 from sklearn.preprocessing import MinMaxScaler
 from keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint
 import numpy as np
+import os
 
 def prepare_data(dataset_name, architecture_type):
     data_loader = UCR_UEA_datasets()
@@ -34,3 +36,27 @@ def prepare_data(dataset_name, architecture_type):
     y_train = to_categorical(y_train - 1, num_classes=num_classes)
     y_test = to_categorical(y_test - 1, num_classes=num_classes)
     return x_train, y_train, x_test, y_test
+
+
+def test_models(model_list, dataset_name, architecture_type):
+    results_path = os.path.join("resultats", dataset_name)
+    os.makedirs(results_path)
+    x_train, y_train, x_test, y_test = prepare_data(dataset_name, architecture_type)
+    results = []
+    
+
+    for i in range(len(model_list)):
+        model = model_list[i]
+        checkpoint_filepath = os.path.join(results_path, f"model_{architecture_type}_{i+1}")
+        model_checkpoint_callback = ModelCheckpoint(
+        filepath=checkpoint_filepath,
+        save_weights=True,
+        monitor='val_loss',
+        mode='min',
+        save_best_only=True)
+
+        print(f"model {i+1}")
+        model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+        model.fit(x=x_train, y=y_train, epochs=20, batch_size=10, validation_data=(x_test, y_test), callbacks=[model_checkpoint_callback])
+        results.append(model)
+    return results
